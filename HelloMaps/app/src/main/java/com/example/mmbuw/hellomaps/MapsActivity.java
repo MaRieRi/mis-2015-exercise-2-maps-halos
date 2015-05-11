5a093bae7ee9df21c28b9604c8ed5ba5af2d82ed
MaRieRi <marie.bornemann@uni-weimar.de>
2015-05-11 18:22:00 +0200

    Beleg 2 fertig;)

--- HelloMaps/app/src/main/java/com/example/mmbuw/hellomaps/MapsActivity.java
+++ HelloMaps/app/src/main/java/com/example/mmbuw/hellomaps/MapsActivity.java
@@ -1,22 +1,59 @@
 package com.example.mmbuw.hellomaps;
 
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLongClickListener,GoogleMap.OnCameraChangeListener {
+
+    public static final String PREF_S = "Lollipop";
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private EditText markerText;
+    private String text;
+    private LatLng position;
+    private TreeSet<String> markerSet=new TreeSet<String>();
+    private  ArrayList<Circle> circlesOp = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        markerText = (EditText)findViewById(R.id.markerText);
         setUpMapIfNeeded();
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnCameraChangeListener(this);
+
+        SharedPreferences markers = PreferenceManager.getDefaultSharedPreferences(this);
+        Set<String> tempSet =markers.getStringSet("MyMapMarkers",markerSet);
+
+        if(!tempSet.isEmpty()){
+            markerSet.addAll(tempSet);
+            Iterator setIter = markerSet.iterator();
+            while (setIter.hasNext()) {
+                String[] s = setIter.next().toString().split(";");
+                 mMap.addMarker(new MarkerOptions().position(new LatLng(Double.parseDouble(s[0]),Double.parseDouble(s[1])))
+                    .title(s[2])
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+         }
+        }
     }
 
     @Override
@@ -60,6 +97,73 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+    }
+
+    @Override
+    public void onMapLongClick(LatLng point) {
+        position=point;
+        text = markerText.getText().toString();
+        mMap.addMarker(new MarkerOptions().position(point).title(text)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+
+        Double pointLat = point.latitude;
+        Double pointLong = point.longitude;
+
+        String textLocation = pointLat.toString()+";"+pointLong.toString()+";"+text;
+        markerSet.add(textLocation);
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition position) {
+
+        if (!circlesOp.isEmpty()) {
+            for (int i = 0; i < circlesOp.size(); ++i) {
+                circlesOp.get(i).remove();
+            }
+        }
+        Location loc = new Location("CameraPo");
+        loc.setLatitude(position.target.latitude);
+        loc.setLatitude(position.target.longitude);
+        if (markerSet!=null) {
+            Iterator setIter = markerSet.iterator();
+            while (setIter.hasNext()) {
+                Location markerLoc = getLocation(setIter.next().toString());
+                if (!isInside(markerLoc)) {
+                    Float dist = loc.distanceTo(markerLoc);
+                    CircleOptions circle = new CircleOptions()
+                            .center(new LatLng(markerLoc.getLatitude(), markerLoc.getLongitude()))
+                            .radius(dist)
+                            .strokeColor(0xFF9932CC);
+
+                    Circle halo = mMap.addCircle(circle);
+                    circlesOp.add(halo);
+                }
+            }
+        }
+    }
+    @Override protected void onStop(){
+        super.onStop();
+        SharedPreferences markers = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor edd = markers.edit();
+        edd.putStringSet("MyMapMarkers",markerSet).apply();
+    }
+
+    private Location getLocation(String s){
+       Location l = new Location("temp");
+        String[] split = s.split(";");
+        l.setLatitude(Double.parseDouble(split[0]));
+        l.setLongitude(Double.parseDouble(split[1]));
+        return l;
+    }
+
+    private boolean isInside(Location loc){
+        boolean test = false;
+        LatLngBounds bounds = mMap.getProjection().getVisibleRegion().latLngBounds;
+        LatLng currentLoc = new LatLng(loc.getLatitude(),loc.getLongitude());
+        if(bounds.contains(currentLoc))
+            test=true;
+        return test;
     }
 }
